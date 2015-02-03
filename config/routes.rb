fcd8c66cb7ef20b697cec512e270109dbaa1f45d
kader <kadersk@gmail.com>
2015-02-03 10:37:05 +0200

    Devise gem integration

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  devise_for :users
+  #get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  #root 'welcome#index'
+
+  root to: "home#index"
 
   resources :articles do
     resources :comments
